d2dbfa4a1c49f97ae08090a133e0f254ae2d4e4a
draxvel <ntv3331998@gmail.com>
2018-08-21 16:58:21 +0300

    Added extension to ImageView.

--- app/src/main/java/com/tkachuk/kedditbysteps/commons/Extensions.kt
+++ app/src/main/java/com/tkachuk/kedditbysteps/commons/Extensions.kt
@@ -1,9 +1,21 @@
 package com.tkachuk.kedditbysteps.commons
 
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+import com.tkachuk.kedditbysteps.R
 
 fun ViewGroup.inflate(layoutId: Int,  attachToRoot: Boolean = false): View {
     return LayoutInflater.from(context).inflate(layoutId, this, false)
+}
+
+fun ImageView.loadImg(imageUrl: String){
+    if(TextUtils.isEmpty(imageUrl)){
+        Picasso.with(context).load(R.mipmap.ic_launcher).into(this)
+    }else{
+        Picasso.with(context).load(imageUrl).into(this)
+    }
 }

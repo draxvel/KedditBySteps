0341b246ebd9dfb81e3fa2c7ea58e1d855c974a8
draxvel <ntv3331998@gmail.com>
2018-08-21 17:04:19 +0300

    Added basic structure of adapters.

--- app/src/main/java/com/tkachuk/kedditbysteps/MainActivity.kt
+++ app/src/main/java/com/tkachuk/kedditbysteps/MainActivity.kt
@@ -4,17 +4,15 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
         if (savedInstanceState == null) {
-            //changeFragment(NewsFragment())
+            changeFragment(NewsFragment())
         }
     }
 
--- app/src/main/java/com/tkachuk/kedditbysteps/NewsFragment.kt
+++ app/src/main/java/com/tkachuk/kedditbysteps/NewsFragment.kt
@@ -6,19 +6,39 @@ import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.tkachuk.kedditbysteps.commons.adapter.NewsAdapter
+import com.tkachuk.kedditbysteps.commons.RedditNewsItem
 import com.tkachuk.kedditbysteps.commons.inflate
 import kotlinx.android.synthetic.main.fragment_news.*
 
 class NewsFragment: Fragment() {
 
-    private val newsList by lazy {
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
         news_list.setHasFixedSize(true)
         news_list.layoutManager = LinearLayoutManager(context)
-        news_list // this will work as the return type
+        initAdapter()
+
+        if(savedInstanceState == null){
+            val news = mutableListOf<RedditNewsItem>()
+            for(i in 1..10){
+                news.add(RedditNewsItem("author$i", "title$i",
+                        i,
+                        32324234234,
+                            "https://picsum.photos/200/200?image=$i", "url$i"))
+            }
+            (news_list.adapter as NewsAdapter).addNews(news)
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = container?.inflate(R.layout.fragment_news)
         return view
+    }
+
+    private fun initAdapter(){
+        if(news_list.adapter == null){
+            news_list.adapter = NewsAdapter()
+        }
     }
 }

482b67f93ac09a9f41315a261633890dd078877b
draxvel <ntv3331998@gmail.com>
2018-08-21 17:00:20 +0300

    Added utils to adapter (constants, view types, delegate adapter)

---
+++ app/src/main/java/com/tkachuk/kedditbysteps/commons/adapter/util/AdapterConstants.kt
@@ -0,0 +1,6 @@
+package com.tkachuk.kedditbysteps.commons.adapter.util
+
+object AdapterConstants {
+    const val NEWS = 1
+    const val LOADING = 2
+}
---
+++ app/src/main/java/com/tkachuk/kedditbysteps/commons/adapter/util/ViewType.kt
@@ -0,0 +1,5 @@
+package com.tkachuk.kedditbysteps.commons.adapter.util
+
+interface ViewType {
+    fun getViewType(): Int
+}

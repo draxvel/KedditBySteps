ae552df12303f4ccf7cbd9df4536911802e64db7
draxvel <ntv3331998@gmail.com>
2018-08-22 10:56:29 +0300

    Added Api classes.

---
+++ app/src/main/java/com/tkachuk/kedditbysteps/api/RestApi.kt
@@ -0,0 +1,22 @@
+package com.tkachuk.kedditbysteps.api
+
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+class RestApi {
+
+    private val redditApi: RedditApi
+
+    init {
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://www.reddit.com")
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build()
+        redditApi = retrofit.create(RedditApi::class.java)
+    }
+
+    fun getNews(after: String, limit: String): Call<RedditDataResponse> {
+        return redditApi.getTop(after, limit)
+    }
+}
